16202718fee8a85baa516ab4fabd6222d1247733
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-03-21 10:58:53 +0200

    Implement routing with BrowserRouter and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
+import ListingsPage from './pages/ListingsPage';
+import ServicesPage from './pages/ServicesPage';
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/listings" element={<ListingsPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
